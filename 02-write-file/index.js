e6ad2f411539938c2834f54bc465b187a935ae55
YaGrig <yashagrigorev@gmail.com>
2022-05-24 12:32:03 +0300

    fix: add goodbye message to task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
-let array = [];
 
-fs.writeFile(
-    path.join(__dirname, 'text.txt'),
-    '',
-    (err) => {
-        if (err) throw err;
-        console.log('Введите текст');
-    }
-);
-stdin.on('data', data => fs.appendFile(
-    path.join(__dirname, 'text.txt'),
-    data,
-    err => {
-        if (err) throw err;
-    }
-));
-// array.forEach(item => );
+console.log('введите текст')
+
+let writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+stdin.on('data', data => {
+    if(data.toString() == 'exit\n'){
+        writeStream.end()} else{
+    writeStream.write(data)};   
+});
+writeStream.on('finish', (err) => {
+    console.log('wrote all data to file');
+    process.exit();
+
+});
+process.on('SIGINT', () => {
+    console.log('wrote all data to file');
+    process.exit();
+});
